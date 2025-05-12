307433c54393a599e692884cfcf88a833c74575b
lordpluha <Tesluakevlad@gmail.com>
2025-05-12 16:29:47 +0200

    Modifications

--- back/index.js
+++ back/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+console.log('Hello from the back-end!');
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
