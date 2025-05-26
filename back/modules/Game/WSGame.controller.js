283cfd5265b5cde14856d64a1c57b57100c959b2
lordpluha <Tesluakevlad@gmail.com>
2025-05-26 08:40:34 +0200

    Add console logging

--- back/modules/Game/WSGame.controller.js
+++ back/modules/Game/WSGame.controller.js
@@ -14,14 +14,7 @@ const broadcastWS = (gameId, payload) => {
 };
 
 wss.on("connection", (ws, req) => {
-  console.log("🔌 WebSocket client connected");
-
-	// извлекаем gameId из URL подключения
-  const qs = req.url.split("?")[1] || "";
-  const params = new URLSearchParams(qs);
-  ws.gameId = params.get("gameId") || null;
-
-  console.log("🔌 WS client subscribed to game:", ws.gameId);
+  console.log(`🔌 WebSocket client connected`);
 
   ws.on("message", async (raw) => {
     let msg, userId;
@@ -37,22 +30,26 @@ wss.on("connection", (ws, req) => {
         case "createGame": {
           const game = await GameService.createGame(userId);
           broadcastWS(game.id, { event: "gameCreated", game });
+          console.log(`[${userId}] Game created with Id = `, game.id);
           break;
         }
         case "joinGame": {
           const game = await GameService.joinGame(userId, msg.payload.gameId);
           broadcastWS(game.id, { event: "playerJoined", game });
+					console.log(`[${userId}] Joined to game with Id = `, game.id);
           break;
         }
         case "startGame": {
           const game = await GameService.startGame(userId, msg.payload.gameId);
           broadcastWS(game.id, { event: "gameStarted", game });
+					console.log(`[${userId}] Start game with Id = `, game.id);
           break;
         }
         case "playCard": {
           const { gameId, cardId, targetId } = msg.payload;
           const game = await GameService.playCard(userId, gameId, cardId, targetId);
           broadcastWS(game.id, { event: "cardPlayed", game, cardId, targetId });
+					console.log(`[User:${userId} - Game:${game.id}] Play card with Id = `, cardId);
           if (game.status === "ENDED") {
             broadcastWS(game.id, { event: "gameEnded", winner: game.winner_id });
           } else {
@@ -65,6 +62,7 @@ wss.on("connection", (ws, req) => {
           const game = await GameService.endTurn(userId, gameId);
           broadcastWS(game.id, { event: "turnEnded", prevPlayer: userId });
           broadcastWS(game.id, { event: "turnStarted", nextPlayer: game.game_state.currentTurn });
+					console.log(`[User:${userId} - Game:${game.id}] End turn`);
           break;
         }
         case "getGame": {
@@ -76,6 +74,7 @@ wss.on("connection", (ws, req) => {
           const game = await GameService.surrenderGame(userId, msg.payload.gameId);
           broadcastWS(game.id, { event: "playerSurrendered", player: userId });
           broadcastWS(game.id, { event: "gameEnded", winner: game.winner_id });
+					console.log(`[User:${userId} - Game:${game.id}] Surrendered`);
           break;
         }
         case "getGameByCode": {
