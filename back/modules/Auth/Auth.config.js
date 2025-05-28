8da387399493c14403b6d26d31ad0da861e9af53
lordpluha <Tesluakevlad@gmail.com>
2025-05-29 01:33:08 +0200

    Revert access token lifetime

--- back/modules/Auth/Auth.config.js
+++ back/modules/Auth/Auth.config.js
@@ -1,8 +1,8 @@
 import { NODE_ENV } from '../../config.js'
 
 const AuthConfig = {
-  // Compare with .env.dev!!!
-  AccessMaxAge: 3 * 60 * 1000, // 3m
+  // Compare with .env
+  AccessMaxAge: 24 * 60 * 60 * 1000, // 1d
   RefreshMaxAge: 7 * 24 * 60 * 60 * 1000, // 7d
 
   cookieOpts: {
